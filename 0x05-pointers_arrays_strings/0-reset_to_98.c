ae9f3c38f2817dcf5d464688cfe0f0132a99a1ff
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 09:35:05 -0700

    takes the pointer and updates the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
+
 /**
- * *  reset_to_98 - takes the pointer and updates the value
- * *  @n: int to check
- * *  Owned By Bwave/Bright
- * *  Return: 0 is success
- * */
+ *  * reset_to_98 - main
+ *   * @n: - pinter to int
+ *    */
 
 void reset_to_98(int *n)
 {
-		*n = 98;
+	*n = 98;
 }
