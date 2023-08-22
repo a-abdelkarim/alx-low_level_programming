cfac148e6043ba7411f9aa51f5ba12eed3e6b87d
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 10:10:49 -0700

    takes the pointer and updates the value

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,27 @@
 #include "main.h"
 /**
- *  * puts2 - print our even char in our string
+ *  * puts_half - print our even char in our string
  *   * @str: pointer to our string
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i;
+	int i, size;
 
+	size = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		if (size % 2 == 0)
+		{
+			if (i >= (size / 2))
+				_putchar(str[i]);
+		}
+		else
+		{
+			if (i > (size / 2))
+				_putchar(str[i]);
+		}
 	}
 	_putchar(10);
 }
