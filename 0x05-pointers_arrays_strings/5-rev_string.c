70d75b34a06a0250965ef6acfc09db8e706d68b2
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 10:02:24 -0700

    takes the pointer and updates the value

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+/**
+ *  * rev_string - print our string in reverse
+ *   * @s: pointer to our string
+ */
+void rev_string(char *s)
+{	
+	int i, size, j;
+	char temp;
+
+	size = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		size++;
+
+	for (j = 0; j < size; j++)		
+	{				
+		temp = s[j];	
+		s[j] = s[size - 1];
+		s[size - 1] = temp;
+		size--;
+	}
+}
