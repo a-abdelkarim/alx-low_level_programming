a8e5f08d61d534db36b3a17e7e3acb090c24aefc
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 09:49:15 -0700

    takes the pointer and updates the value

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * print_rev - print our string in reverse
+ *   * @s: pointer to our string
+ */
+void print_rev(char *s)
+{
+		int i, size;
+
+			size = 0;
+
+				for (i = 0; s[i] != '\0'; i++)
+						{
+									size++;
+										}
+					size--;
+						for (i = size; i >= 0; i--)
+									_putchar(s[i]);
+							_putchar(10);
+}
