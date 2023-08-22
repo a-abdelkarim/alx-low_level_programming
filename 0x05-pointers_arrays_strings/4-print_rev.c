37cb1de5bd18802979bb3c7c534b0a3102a1a0b7
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 09:57:03 -0700

    takes the pointer and updates the value

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,20 +7,14 @@
 void print_rev(char *s)
 {
 	int i, size;
-
 	size = 0;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		size++;
-		
 	}
 	
 	size--;
-
 	for (i = size; i >= 0; i--)
-
 		_putchar(s[i]);
-
 	_putchar(10);
 }
