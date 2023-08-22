25a68d4a51cfec89e4fd28ef23a7f9c288690c50
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-08-22 09:44:05 -0700

    takes the pointer and updates the value

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * _strlen - function to know what is the string size
+ *   * @s: pointer to our string
+ *    * Return: size of string or array of char
+ *     */
+int _strlen(char *s)
+{
+	int i, string_size = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		string_size++;
+	}
+	return (string_size);
+}
