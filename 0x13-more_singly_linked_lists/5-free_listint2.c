783a4234363c95a14ad9c029ec8d7c2e3e10b00d
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-25 11:40:48 -0700

    hot fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-
+	listint_t *next;
+	
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
-		current = *head;
 		*head = (*head)->next;
 		free(current);
+		*head = next;
 	}
 }
