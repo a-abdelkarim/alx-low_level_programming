a8d8d0df5d6e8abf3a850050f3ebe83658b6de96
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-25 09:01:26 -0700

    hot fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,15 +11,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0; // Initialize count to 0
+    	size_t count = 0; // Initialize count to 0
+   
+	while (h != NULL) // Traverse the linked list
+    	{
+    		printf("%d\n", h->n); // Print the value of the current node
+	     	h = h->next; // Move to the next node
+	       	count++; // Increment the count
+	}
 
-    while (h != NULL) // Traverse the linked list
-    {
-        printf("%d\n", h->n); // Print the value of the current node
-        h = h->next; // Move to the next node
-        count++; // Increment the count
-    }
-
-    return (count); // Return the number of nodes
+	return (count); // Return the number of nodes
 }
-
