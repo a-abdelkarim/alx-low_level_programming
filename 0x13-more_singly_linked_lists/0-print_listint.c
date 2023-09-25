78ca93786c82ffd9dfa9d23f61eb320c32b4b00a
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-25 08:52:48 -0700

    hot fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_linked_list - prints all the elements of a linked list of integers.
+ * print_listint - prints all the elements of a listint_t list.
  *
- * @head: pointer to the head of the linked list
+ * @h: head of linklist node
  *
  * Return: the number of nodes
  *
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-    return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
-
