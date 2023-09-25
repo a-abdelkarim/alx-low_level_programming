d8d413f9e139ba0f8ceba99a466cf8809f3db0f4
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-25 00:30:47 -0700

    prints all the elements of a linked list of integers.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_linked_list - prints all the elements of a linked list of integers.
+ *
+ * @head: pointer to the head of the linked list
+ *
+ * Return: the number of nodes
+ *
+ */
+size_t print_linked_list(const struct Node *head)
+{
+    size_t count = 0;
+
+    while (head != NULL)
+    {
+        printf("%d\n", head->data);
+        head = head->next;
+        count++;
+    }
+    return (count);
+}
+
