eb36b5c5128d5349a2570920fa03040c35a8604b
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-25 11:46:30 -0700

    hot fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,6 +4,8 @@
 /**
  * free_listint2 - Frees a listint_t list and sets the head to NULL.
  * @head: Pointer to the pointer of the head of the listint_t list
+ *
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
