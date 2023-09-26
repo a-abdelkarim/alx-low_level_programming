79bc49e4a0f7656e5fca88336c7471efc74adf51
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-26 01:56:30 -0700

    Inserts a new node at a given position in a listint_t list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,29 +20,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if(new_node == NULL)
 		return (NULL);
+	if (idx != 0)
+	{
+		current = *head;
+		for (i = 0, i < idx - 1 && current != NULL; i++)
+		{
+			current = current->next;
+		}
+		if (current == NULL)
+			return (NULL);
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		*head = new_node;
+		*head = new;
 		return (new_node);
 	}
-	current = *head;
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (current == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		current = current -> next;
-	}
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL)
-	}
-	new_node->next = current->next;
+	new_node->next = temp->next;
 	current->next = new_node;
 	return (new_node);
 }
