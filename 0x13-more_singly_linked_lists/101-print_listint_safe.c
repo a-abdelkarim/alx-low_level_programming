bcebf8dabd65b701c40f010941fa47a67732a71c
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-26 06:28:48 -0700

    print_listint_safe - Prints a listint_t linked list safely.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,7 @@
 /**
  * _reallocate_list - reallocates memory for an array of pointers
  * to the nodes in a linked list
- * @list: the old list to append
+ * @old_list: the old list to append
  * @size: size of the new list (always one more than the old list)
  * @new: new node to add to the list
  *
