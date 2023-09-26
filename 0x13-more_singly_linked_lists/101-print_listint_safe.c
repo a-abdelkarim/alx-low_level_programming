1516cf3489d20f73d2738c21cb20bf18bee1085a
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-26 06:40:51 -0700

    print_listint_safe - Prints a listint_t linked list safely.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,13 +5,13 @@
 /**
  * _reallocate_list - reallocates memory for an array of pointers
  * to the nodes in a linked list
- * @old_list: the old list to append
+ * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
  * @new_node: new node to add to the list
  *
  * Return: pointer to the new list
  */
-const listint_t **_reallocate_list(const listint_t **old_list, size_t size, const listint_t *new_node)
+const listint_t **_reallocate_list(const listint_t **list, size_t size, const listint_t *new_node)
 {
 	const listint_t **new_list;
 	size_t i;
@@ -19,13 +19,13 @@ const listint_t **_reallocate_list(const listint_t **old_list, size_t size, cons
 	new_list = malloc(size * sizeof(listint_t *));
 	if (new_list == NULL)
 	{
-		free(old_list);
+		free(list);
 		exit(98);
 	}
 	for (i = 0; i < size - 1; i++)
-		new_list[i] = old_list[i];
+		new_list[i] = list[i];
 	new_list[i] = new_node;
-	free(old_list);
+	free(list);
 	return (new_list);
 }
 
