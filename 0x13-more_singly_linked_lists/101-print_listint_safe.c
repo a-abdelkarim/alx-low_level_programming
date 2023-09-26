a88d5ef5a16134af7ddd757f7e33f3faa520ecce
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-26 06:13:56 -0700

    print_listint_safe - Prints a listint_t linked list safely.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -54,7 +54,7 @@ size_t print_listint_safe(const listint_t *head)
 		}
 		count++;
 		list = _reallocate_list(list, count, head);
-		printf("[%p] %d\n", (void*)head, head->n);
+		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
 	free(list);
