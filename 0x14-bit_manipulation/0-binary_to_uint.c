f2a672fd60fd05d987086a4b7d01c6576fd65e17
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-28 12:47:52 -0700

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,21 +13,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
 	while (b[i] != '\0')
 	{
 		char c = b[i];
 
 		if (c == '0' || c == '1')
-		{
 			result = result * 2 + (c - '0');
-		}
 		else
-		{
 			return (0);
-		}
 		i++;
 	}
 	return (result);
