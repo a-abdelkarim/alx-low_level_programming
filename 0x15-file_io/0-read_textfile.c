b4c3bbb1a92da347bf975b095849bb3e0bf65c8d
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-02 11:38:36 -0700

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	bread = read(fd, buffer, letters);
-	bwritten = write(STDOUT_FILENO, buffer, bwritten);
+	bwritten = write(STDOUT_FILENO, buffer, bread);
 
 	if (fd == -1 || bread == -1 || bwritten == -1 || bwritten != bread)
 	{
