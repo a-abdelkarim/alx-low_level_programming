72d5388ed7973686181e3dbeb35863703e096c4a
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-05 02:55:53 -0700

    hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,8 @@
 
 #define BUFSIZE 1024
 
+void close_file(int file);
+
 /**
  * main - copies the content of one file to another file
  * @argc: the number of cmd arguments
@@ -15,8 +17,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, num_bytes;
+	int fd_from, fd_to, num_bytes, times = 1;
 	char buffer[BUFSIZE];
+	ssize_t read_bytes = 0, write_bytes = 0;
+	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
@@ -31,39 +35,38 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
 
-	while ((num_bytes = read(fd_from, buffer, BUFSIZE)) > 0)
+	while (1)
 	{
-		if (write(fd_to, buffer, num_bytes) != num_bytes)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-
-		if (num_bytes == -1)
+		read_bytes = read(fd_from, buffer, BUFSIZE);
+		if (read_bytes < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(fd_from);
+			close_file(fd_from);
+			exit(98);
+		}
+		write_bytes = write(fd_to, buffer, BUFSIZE);
+		if (write_bytes < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_to);
 			exit(98);
 		}
-
-		if (close(fd_from) == -1 || close(fd_to) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
-			exit(100);
-		}
+		if (read_bytes < BUFSIZE)
+			break;
+		lseek(fd_to, 0, SEEK_END);
+		lseek(fd_from, BUFSIZE * times, SEEK_SET);
+		times++;
 	}
-
+	close_file(fd_from);
+	close_file(fd_to);
 	return (0);
 }
 
