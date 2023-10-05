5d03739370d821cdf89cb4418b35d8c51c8d8999
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-05 02:30:50 -0700

    hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,8 @@
 
 #define BUFSIZE 1024
 
+void close_file(int file);
+
 /**
  * main - copies the content of one file to another file
  * @argc: the number of cmd arguments
@@ -21,14 +23,14 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -36,7 +38,7 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Cannot write to file %s\n", argv[2]);
 		close(fd_from);
-		return (99);
+		exit(99);
 	}
 
 	while ((num_bytes = read(fd_from, buffer, BUFSIZE)) > 0)
@@ -46,7 +48,7 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "ERROR: Cannot write to file %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			return (98);
+			exit(98);
 		}
 
 		if (num_bytes == -1)
@@ -54,15 +56,30 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "ERROR: Cannot read from file %s\n", argv[1]);
 			close(fd_from);
 			close(fd_to);
-			return (98);
+			exit(98);
 		}
 
 		if (close(fd_from) == -1 || close(fd_to) == -1)
 		{
 			dprintf(STDERR_FILENO, "ERROR: Cannont close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
-			return (100);
+			exit(100);
 		}
 	}
 
 	return (0);
+}
+
+/**
+ * close_file - closes a file
+ * @file: pointer to the file
+ */
+void close_file(int file)
+{
+	int result = close(file);
+
+	if (result < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
 }
