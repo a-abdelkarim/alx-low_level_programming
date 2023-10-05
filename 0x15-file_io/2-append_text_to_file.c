e34c32877c7a4fbfa63c3825ad2d81fe8bd02227
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-05 03:23:30 -0700

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,23 +24,24 @@ size_t _strlen(char *str)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int o;
 	ssize_t length;
 
 	if (filename == NULL)
 		return (-1);
 	
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (o < 0)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		length = write(fd, text_content, _strlen(text_content));
+		lseek(o, 0, SEEK_END);
+		length = write(o, text_content, _strlen(text_content));
 	}
-	close(fd);
+	close(o);
 
-	if (length == -1)
+	if (length < 0)
 		return (-1);
 	return (1);
 }
