100eb35ddc89891a8c38e2db8fbaa578450c4034
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-05 02:43:04 -0700

    hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 	{
 		if (write(fd_to, buffer, num_bytes) != num_bytes)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(98);
