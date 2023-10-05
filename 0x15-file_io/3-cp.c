190714b44586bb88646a2b9c7d35ec368b12e70c
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-05 02:37:12 -0700

    hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ void close_file(int file);
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, times = 1;
+	int fd_from, fd_to, num_bytes, times = 1;
 	size_t read_bytes = 0, write_bytes = 0;
 	char buffer[BUFSIZE];
 	mode_t permissions = S_IRUSR | S_IWUSR | I_IRGRP | S_IWGRP | S_IROTH;
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cannot write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 	{
 		if (write(fd_to, buffer, num_bytes) != num_bytes)
 		{
-			dprintf(STDERR_FILENO, "ERROR: Cannot write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(98);
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 
 		if (num_bytes == -1)
 		{
-			dprintf(STDERR_FILENO, "ERROR: Cannot read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(fd_from);
 			close(fd_to);
 			exit(98);
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 
 		if (close(fd_from) == -1 || close(fd_to) == -1)
 		{
-			dprintf(STDERR_FILENO, "ERROR: Cannont close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
 			exit(100);
 		}
 	}
